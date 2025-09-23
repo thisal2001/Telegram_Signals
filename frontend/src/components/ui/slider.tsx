d0048407343358bf1b66e4fe431aca151574476d
thisal2001 <thisal38698@gmail.com>
2025-09-23 22:27:55 +0530

    update slider

--- frontend/src/components/ui/slider.tsx
+++ frontend/src/components/ui/slider.tsx
@@ -2,26 +2,37 @@
 
 import * as React from "react"
 
-interface SliderProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  value: number[]
-  onValueChange: (value: number[]) => void
-  min?: number
-  max?: number
-  step?: number
+interface SliderProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'defaultValue'> {
+    value: number[]
+    onValueChange: (value: number[]) => void
+    min?: number
+    max?: number
+    step?: number
 }
 
-export function Slider({ value, onValueChange, min = 0, max = 100, step = 1, ...props }: SliderProps) {
-  return (
-    <input
-      type="range"
-      value={value[0]}
-      min={min}
-      max={max}
-      step={step}
-      onChange={(e) => onValueChange([Number(e.target.value)])}
-      {...props}
-    />
-  )
-}
+const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
+    ({ className, value, onValueChange, min = 0, max = 100, step = 1, ...props }, ref) => {
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            const newValue = [parseFloat(e.target.value)]
+            onValueChange(newValue)
+        }
 
+        return (
+            <input
+                type="range"
+                min={min}
+                max={max}
+                step={step}
+                value={value[0] || min}
+                onChange={handleChange}
+                className={`flex w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 ${className || ''}`}
+                ref={ref}
+                {...props}
+            />
+        )
+    }
+)
 
+Slider.displayName = "Slider"
+
+export { Slider }
